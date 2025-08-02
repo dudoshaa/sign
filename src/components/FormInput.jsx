73985d7df4eb0082a5062b3a0d82ad534e25f4b9
durdona <durdona>
2025-08-02 15:09:03 +0500

    added responsiv design

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,11 +4,12 @@ import { PiEyeClosedBold, PiEyeBold } from "react-icons/pi";
 function FormInput({ label, type, name, icon: Icon }) {
   const [show, setShow] = useState(false);
   const isPassword = type === "password";
+
   return (
-    <fieldset className="mb-6">
+    <fieldset className="mb-6 w-full">
       <div className="relative w-full">
         {Icon && (
-          <Icon className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+          <Icon className="absolute left-3 top-1/2 -translate-y-1/2 text-white w-5 h-5" />
         )}
 
         <input
@@ -17,12 +18,13 @@ function FormInput({ label, type, name, icon: Icon }) {
           placeholder={label}
           className={`w-full py-2 ${Icon ? "pl-10" : "pl-4"} ${
             isPassword ? "pr-10" : ""
-          } border border-gray-300 rounded-3xl focus:outline-none focus:ring-2 focus:ring-yellow-400`}
+          } bg-transparent text-white placeholder-white border border-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-yellow-400`}
         />
+
         {isPassword && (
           <div
             onClick={() => setShow(!show)}
-            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer"
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-white cursor-pointer"
           >
             {show ? <PiEyeClosedBold /> : <PiEyeBold />}
           </div>
